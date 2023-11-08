aff07c2969aba4a4ebe95472de224dc9c4934760
Vikas Kumar <vikasbihari6.com>
2023-11-08 08:18:38 +0530

    commit changes

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -4,6 +4,10 @@ import project13 from "../assets/project13.png"
 import project21 from "../assets/project21.png"
 import project22 from "../assets/project22.png"
 import scribble from "../assets/scribble.png"
+import colorGame from "../assets/colorGame.png"
+import addTo from "../assets/addToByReact.png"
+import incrDec from "../assets/incrementDecrement.png"
+import numGuess from "../assets/numberGuess.png"
 const ProjectCardData=[
     {
         imgsrc: project11,
@@ -25,6 +29,30 @@ const ProjectCardData=[
         text:"Developed a minor project which can add some title and content with Date",
         view:"https://vikas-scribble.onrender.com/",
         sorce:"https://github.com/vikasbihari6/Scribble"
+    },{
+        imgsrc: colorGame,
+        title:"Color Game",
+        text:"Developed a game which has two difficalty.In easy there are 3 boxes and in hard there are 6 boxes",
+        view:"https://vikasbihari6.github.io/ColorGame/",
+        sorce:"https://github.com/vikasbihari6/ColorGame"
+    },{
+        imgsrc: numGuess,
+        title:"Number Guessing Game",
+        text:"Developed a minor project in which player has to guess the number between 1 and 100.",
+        view:"https://vikasbihari6.github.io/NumberGuessingGame/",
+        sorce:"https://github.com/vikasbihari6/NumberGuessingGame"
+    },{
+        imgsrc: addTo,
+        title:"Add to by React",
+        text:"Developed a minor project which can add paragraph and can delete on click.",
+        view:"",
+        sorce:"https://github.com/vikasbihari6/addTobyreact"
+    },{
+        imgsrc: incrDec,
+        title:"Increment and Decrement by React-Redux",
+        text:"Developed a minor project for learning redux which can increment and decrement.",
+        view:"",
+        sorce:"https://github.com/vikasbihari6/incrementDecrementbyReact"
     },
 
 ]
